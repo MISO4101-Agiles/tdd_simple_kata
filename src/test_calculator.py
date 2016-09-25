353927a91db847c1b08ea670188afa39b39d5f8d
lstalero21 <ls.talero21@uniandes.edu.co>
2016-09-25 17:16:27 -0500

    Paso 2 - Se crea el test para cuando la cadena contiene un numero (ROJO)

--- src/test_calculator.py
+++ src/test_calculator.py
@@ -7,3 +7,6 @@ __author__ = 'Luis_Sebastian_Talero'
 class TestCalculator(TestCase):
     def test_sumar(self):
         self.assertEqual(Calculator.sumar(""), 0, "Empty String")
+
+    def test_sumar_una_cadena(self):
+        self.assertEqual(Calculator.sumar("1"), 1, "One number")
