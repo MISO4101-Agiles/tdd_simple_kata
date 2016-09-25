db319f376d3730b94efdc16df80ead77fc67b053
lstalero21 <ls.talero21@uniandes.edu.co>
2016-09-25 17:57:47 -0500

    Paso 2 - Se crea el test para cuando la cadena contiene dos numeros (ROJO)

--- src/test_calculator.py
+++ src/test_calculator.py
@@ -11,3 +11,6 @@ class TestCalculator(TestCase):
     def test_sumar_una_cadena(self):
         self.assertEqual(Calculator.sumar("1"), 1, "One number")
         self.assertEqual(Calculator.sumar("2"), 2, "One number")
+
+    def test_sumar_dos_cadenas(self):
+        self.assertEqual(Calculator.sumar("1,1"), 2, "One number")
